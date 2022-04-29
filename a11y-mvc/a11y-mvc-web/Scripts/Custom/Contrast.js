0fdca81e5c2d0db6f5cb52868ec5027f1553a14a
Andrii Solovienko <osobuy@gmail.com>
2022-04-29 12:17:05 +0300

    save to localStorage

--- a11y-mvc/a11y-mvc-web/Scripts/Custom/Contrast.js
+++ a11y-mvc/a11y-mvc-web/Scripts/Custom/Contrast.js
@@ -1,20 +1,89 @@
 ﻿$(document).ready(() => {
-    var contrast = JSON.parse(window.localStorage.getItem("contrast"));
-    if (contrast) {
-        console.log(contrast.isEnabled);
-    }
+    const KEY = "accSettings";
+    const settings = readStorage();
 
-    $("#ContrastButton").click(function () {
+    var isContrastOn = settings.isContrastOn;
+    var scale = parseInt(settings.scale);
+    const maxScale = 4;
+
+    if (isContrastOn) {
+        applyContrast();
+    }
+    if (scale > 0) {
+        // Move to the method
         var headers = document.querySelectorAll("h3,h1,h2,p,a");
         for (let i = 0; i < headers.length; i++) {
-
             let el = headers[i];
             let style = window.getComputedStyle(el, null).getPropertyValue('font-size');
             let font = parseFloat(style);
-
-            el.style.fontSize = (font + 1) + 'px';
+            el.style.fontSize = (font + 4 * scale) + 'px';
         }
+    }
 
-        window.localStorage.setItem("contrast", JSON.stringify({ "isEnabled": true }))
+
+    $("#ContrastButton").click(function () {
+        const st = document.head.querySelector("#atUser");
+        if (isContrastOn) {
+            st.remove();
+        } else {
+            applyContrast();
+        }
+        isContrastOn = !isContrastOn;
+        writeStorage();
     });
+
+    $("#FontPlusButton").click(function () {
+        console.log("1 scale : ", scale);
+        if (scale <= maxScale) {
+            var headers = document.querySelectorAll("h3,h1,h2,p,a");
+            for (let i = 0; i < headers.length; i++) {
+                let el = headers[i];
+                let style = window.getComputedStyle(el, null).getPropertyValue('font-size');
+                let font = parseFloat(style);
+                el.style.fontSize = (font + 4) + 'px';
+            }
+            scale++;
+            writeStorage();
+        }
+    });
+
+    $("#FontMinusButton").click(function () {
+        if (scale > 0) {
+            var headers = document.querySelectorAll("h3,h1,h2,p,a");
+            for (let i = 0; i < headers.length; i++) {
+
+                let el = headers[i];
+                let style = $(el).css("font-size");
+                let font = parseFloat(style);
+
+                $(el).css("font-size", (font - 4) + "px");
+            }
+            scale--;
+            writeStorage();
+        }
+    });
+
+    function applyContrast() {
+        const link = document.createElement("link");
+        link.setAttribute("rel", "stylesheet");
+        link.setAttribute("id", "atUser");
+        link.setAttribute("type", "text/css");
+        link.setAttribute("href", "Scripts/Angular/assets/style-a11y.css")
+        document.head.appendChild(link);
+    }
+
+    function writeStorage() {
+        window.localStorage.setItem(KEY, JSON.stringify({ isContrastOn: isContrastOn, scale: scale }));
+    }
+
+    function readStorage() {
+        const value = window.localStorage.getItem(KEY);
+        if (value) {
+            return JSON.parse(value);
+        }
+        else {
+            return { isContrastOn: false, scale: 0 };
+        }
+    }
 });
+
